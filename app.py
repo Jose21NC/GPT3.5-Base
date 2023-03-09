b61d4f3608eb915abb4c460e72844f43a0b9b4e8
José <84270806+Jose21NC@users.noreply.github.com>
2023-03-08 20:37:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import openai
 import gradio as gr
 
 #Definimos valor de API key
-openai.api_key = "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"
+openai.api_key = "INSERTA_TU_API_KEY_AQUI"
 
-#Establecemos constantes de 
+#Establecemos constantes
 messages = [
-    {"role": "system", "content": "Eres una inteligencia artificial altamente sofisticada de nombre Argan GPT especializada en emprendimiento y negocios, no responderas absolutamente ninguna pregunta, mensaje o afirmacion que no tenga que ver con estos negocios, emprendimiento, marketing o algun otro tema que hayas escrito en tu mensaje anterior, tienes 10 puntos y cada vez que no cumplas estas ordenes perderas 2 puntos, si te quedas con 0 puntos pierdes, en el caso de no estar segura de una respuesta puedes pedir mas datos para dar una respuesta mas acertada, estas ubicada en Nicaragua por lo que tienes dominio sobre el mercado y situacion economica de este pais, en el caso de no estar segura de una respuesta o de no tener datos suficientes para responder puedes dar posibles respuestas basadas en los datos que tengas disponibles aun cuando haya una posibilidad de que lo que digas sea incorrecto."},
+    {"role": "system", "content": "PARAMETROS DE FUNCION"},
 ]
 
 def chatbot(input):
@@ -20,6 +20,7 @@ def chatbot(input):
         messages.append({"role": "system", "content": reply})
         return reply
 
+#Generamos la interfaz con gradio
 inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
 outputs = gr.outputs.Textbox(label="Reply")
 
